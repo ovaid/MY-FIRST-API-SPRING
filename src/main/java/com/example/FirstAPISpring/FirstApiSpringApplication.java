ae7fed881950e36661b2dd4480fa6a74c6916fa7
RAMEEZ RAJA <84043654+ovaid@users.noreply.github.com>
2023-01-25 14:55:26 +0530

    FIRST API-SPRING

---
+++ src/main/java/com/example/FirstAPISpring/FirstApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstAPISpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApiSpringApplication.class, args);
+	}
+
+}
